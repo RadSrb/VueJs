cf3398dddc82112eb799d95444404a362d37d6c3
RadSrb <alex.radic@gmx.at>
2022-12-12 19:34:06 +0100

    simple

---
+++ vue/simple_calclator/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <the-calculator></the-calculator>
+</template>
+
+<script>
+import TheCalculator from './components/TheCalculator.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TheCalculator,
+  }
+}
+</script>
+
+<style>
+body{
+  margin:0px;
+}
+</style>
